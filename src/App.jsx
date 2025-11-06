62d0382739eab3ad231cd988e2f87c5be294ca93
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2025-11-06 14:24:22 +0530

    About section has been started and yet to work.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import About from './components/About';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -9,6 +10,7 @@ const App = () => {
     <div className='font-sans overflow-x-hidden'>
       <Navbar />
       <Banner />
+      <About />
     </div>
   )
 }
