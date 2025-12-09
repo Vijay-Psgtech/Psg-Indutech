c12f1de244d6d7b6303f4e154a34994258ef0316
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2025-12-09 14:57:21 +0530

    Mission section items psoitioning has been changed.

--- src/components/data/MissionItems.jsx
+++ src/components/data/MissionItems.jsx
@@ -1,11 +1,19 @@
 export const missionItems = [
-  { title: "Resource Center", color: "from-pink-400 to-pink-600", deg: 300 },
+  { 
+    title: "Resource Center", 
+    color: "from-pink-400 to-pink-600", 
+    deg: 330 
+  },
   {
     title: "Testing Capabilities",
     color: "from-orange-400 to-orange-600",
-    deg: 240,
+    deg: 270,
   },
-  { title: "Consultancy", color: "from-sky-400 to-sky-600", deg: 180 },
+  { 
+    title: "Consultancy", 
+    color: "from-sky-400 to-sky-600", 
+    deg: 15 
+  },
   {
     title: "Training & Workshops",
     color: "from-red-400 to-red-600",
@@ -16,10 +24,14 @@ export const missionItems = [
     color: "from-blue-400 to-blue-600",
     deg: 60,
   },
-  { title: "Pilot Facility", color: "from-yellow-400 to-yellow-600", deg: 0 },
+  { 
+    title: "Pilot Facility", 
+    color: "from-yellow-400 to-yellow-600", 
+    deg: 210 
+  },
   {
     title: "Incubation Center",
     color: "from-emerald-400 to-emerald-600",
-    deg: 330,
+    deg: 165,
   },
 ];
