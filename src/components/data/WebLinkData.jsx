ed76106209bea43df894047bd676370ee4e70a3c
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2025-12-16 09:32:53 +0530

    Web links of Coe page work has been done.

---
+++ src/components/data/WebLinkData.jsx
@@ -0,0 +1,52 @@
+export const linksData = [
+  {
+    id: 1,
+    category: "Centre of Excellence for Agrotech",
+    linkText: "The Synthetic & Art Silk Mills’ Research Association, Mumbai",
+    url: "https://www.sasmira.org",
+  },
+  {
+    id: 2,
+    category: "Centre of Excellence for Geotech",
+    linkText: "The Bombay Textile Research Association, Mumbai",
+    url: "https://www.btraindia.com",
+  },
+  {
+    id: 3,
+    category: "Centre of Excellence for Meditech",
+    linkText: "South India Textile Research Association, Coimbatore",
+    url: "https://www.sitra.org.in",
+  },
+  {
+    id: 4,
+    category: "Centre of Excellence for Composites",
+    linkText: "Ahmedabad Textile Industry’s Research Association, Ahmedabad",
+    url: "https://www.atira.in",
+  },
+  {
+    id: 5,
+    category: "Centre of Excellence for Indutech",
+    linkText:
+      "Department of Textile & Fashion Technology, PSG College of Technology",
+    url: "#",
+  },
+  {
+    id: 6,
+    category: "Centre of Excellence for Nonwovens",
+    linkText:
+      "DKTE COE in Nonwovens, DKTES Textile & Engineering Institute, Kolhapur",
+    url: "http://www.dktecoenonwovens.in/",
+  },
+  {
+    id: 7,
+    category: "Centre of Excellence for Protech",
+    linkText: "Northern India Textile Research Association (NITRA), Ghaziabad",
+    url: "https://nitratextile.org/",
+  },
+  {
+    id: 8,
+    category: "Centre of Excellence for Sportech",
+    linkText: "www.wraindia.com/sportech1",
+    url: "http://www.wraindia.com/sportech1/index.htm",
+  },
+];
