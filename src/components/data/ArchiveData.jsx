f7e07dd5d4237a9976919994643629c23ee340ee
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2025-12-20 14:11:56 +0530

    Archives docs has been added.

--- src/components/data/ArchiveData.jsx
+++ src/components/data/ArchiveData.jsx
@@ -2,44 +2,44 @@ export const archiveItems = [
   {
     id: 1,
     title: "Tender FIC 6 Corrigendum",
-    link: "#",
+    link: "/docs/archives/FIC 6  document 15.02.2017.pdf",
   },
   {
     id: 2,
     title: "Tender FIC 5 Corrigendum",
-    link: "#",
+    link: "/docs/archives/FIC 5 corrigendum document 16.12.2016.pdf",
   },
   {
     id: 3,
     title: "Tender 11 FIC 4",
-    link: "#",
+    link: "/docs/archives/Tender FIC 4 document with corrigendum  1.pdf",
   },
   {
     id: 4,
     title: "Tender 11 FIC 3",
-    link: "#",
+    link: "/docs/archives/Tender 11   FIC 3 document with corrigend.pdf",
   },
   {
     id: 5,
     title:
       "Report on Advanced Porometry and its Innovative Applications in Filtration Industry",
-    link: "#",
+    link: "/docs/archives/ADVANCED POROMETRY AND ITS INNOVATIVE APP.pdf",
   },
   {
     id: 6,
     title:
       'A Report on One Day Course on "Manufacturing of Non Woven Fabrics in Needle Punching Technology"',
-    link: "#",
+    link: "/docs/archives/Needle punching course report.pdf",
   },
   {
     id: 7,
     title:
       'A Report on One Day Training Program on "Universal Testing Machines for Technical Textiles"',
-    link: "#",
+    link: "/docs/archives/Report on UTM Program.pdf",
   },
   {
     id: 8,
     title: "Tender 12 & FIC 7 - Tender document 30.08.2017",
-    link: "#",
+    link: "/docs/archives/Tender 12 and FIC 7_30.08.2017 - 27.09.20.pdf",
   },
 ];
