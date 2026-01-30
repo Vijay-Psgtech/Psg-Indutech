bfb29ee1b2e2a42d64fe6a3ffe7a536fc641fb9e
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-01-30 09:28:58 +0530

    Res-center path and routes changes & facilities files has been removed.

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,19 @@ import Loader from "./components/Loader";
 const Layout = lazy(() => import("./components/Layout"));
 const HomePage = lazy(() => import("./components/HomePage"));
 const PsgCoEAbout = lazy(() => import("./pages/about/PsgCoEAbout"));
-const TrainingPage = lazy(() => import("./pages/trainingFacility/TrainingPage"));
-const Products = lazy(() => import("./pages/products/Products"));
-const IncubationPrototype = lazy(() => import("./pages/facilities/IncubationPrototype"));
-const ProductDevelopment = lazy(() => import("./pages/facilities/ProductDevelopment"));
+
 const Testing = lazy(() => import("./pages/testingFacility/Testing"));
-const CommericalProduction = lazy(() => import("./pages/facilities/CommericalProduction"));
-const HotMeltLamination = lazy(() => import("./pages/facilities/HotMeltLamination"));
-const ResourceCenterCapabilities = lazy(() => import("./pages/facilities/ResourceCenter"));
+const TrainingPage = lazy(() => import("./pages/trainingFacility/TrainingPage"));
+const ResourceCenterCapabilities = lazy(() => import("./pages/resourceCenter/ResourceCenter"));
+const Products = lazy(() => import("./pages/products/Products"));
 const GalleryAlbum = lazy(() => import("./pages/gallery/GalleryAlbum"));
 const AlbumPage = lazy(() => import("./pages/gallery/AlbumPage"));
 const ContactPage = lazy(() => import("./pages/contact/ContactUs"));
+
 const Page404 = lazy(() => import("./components/page404"));
+
+
+
 
 const App = () => {
   return (
@@ -30,18 +31,17 @@ const App = () => {
             <Route path="" element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="/psg-coe-about" element={<PsgCoEAbout />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/inc-pro" element={<IncubationPrototype />} />
-            <Route path="/prod-dev" element={<ProductDevelopment />} />
+
             <Route path="/testing" element={<Testing />} />
-            <Route path="/comm-prod" element={<CommericalProduction />} />
-            <Route path="/hot-melt" element={<HotMeltLamination />} />
             <Route path="/training" element={<TrainingPage />} />
             <Route path="/res-center" element={<ResourceCenterCapabilities />} />
+            <Route path="/products" element={<Products />} />
             <Route path="/gallery" element={<GalleryAlbum />} />
             <Route path="/gallery/:id" element={<AlbumPage />} />
             <Route path="/contact" element={<ContactPage />} />
-            <Route path="/coming-soon" element={<Page404 />} />
+
+            <Route path="*" element={<Page404 />} />
+            
             </Route>
           </Routes>
         </Suspense>
