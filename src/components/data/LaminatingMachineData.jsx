2bd3a023b7574e296775c24c7f955ef7371ba204
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-01-31 09:49:27 +0530

    Laminating Machine data contents has been added as separate json file.

---
+++ src/components/data/LaminatingMachineData.jsx
@@ -0,0 +1,41 @@
+export const applications = [
+  "Layering of Materials",
+  "Bed Dry Sheets",
+  "Garment Interlinings",
+  "Mattress Covers",
+  "Military Suits",
+  "Seat Coverings",
+];
+
+export const productApplications = [
+  "Baby garments",
+  "Rainwear",
+  "Cold weather jackets",
+  "Coveralls",
+  "Ironing boards",
+];
+
+export const materials = [
+  "Woven fabrics",
+  "Knitted fabrics",
+  "Polar fleece",
+  "Terry fabrics",
+  "Nonwoven materials",
+  "Mesh fabrics",
+  "Foams",
+];
+
+export const advantages = [
+  "Superior finishing quality",
+  "Capability to laminate any two flexible rollform materials",
+];
+
+/* ======================== */
+
+export const specifications = [
+  { label: "Working Width", value: "1200 – 2200 mm" },
+  { label: "Roller Width", value: "2400 mm" },
+  { label: "Speed", value: "3 – 40 m/min" },
+  { label: "Production Capacity", value: "2000 – 15,000 meters/shift" },
+  { label: "Raw Materials", value: "PUR, TPU Film, PE Film" },
+];
